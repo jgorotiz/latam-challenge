2677acfd88b468d31fe8cc9d807d62d9881eadff
Jonathan Gorotiza <jgorotiz@espol.edu.ec>
2024-03-16 11:40:40 -0500

    code for q2 time

--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,27 @@
 from typing import List, Tuple
+import emoji
+import json
+from collections import Counter
+
+
+def extraer_emojis(texto):
+    todos_emojis = [car for car in texto if car in emoji.EMOJI_DATA]
+    return todos_emojis
+
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    contador_emojis = Counter()
+    with open(file_path, 'r', encoding='utf-8') as archivo:
+        for linea in archivo:
+            tweet = json.loads(linea)
+            # Extraer el texto del tweet
+            texto_tweet = tweet['content']
+            # Encontrar y contar todos los emojis en el texto
+            emojis_encontrados = extraer_emojis(texto_tweet)
+            contador_emojis.update(emojis_encontrados)
+
+    resultado = []
+    for emoji, conteo in contador_emojis.most_common(10):
+        resultado.append((emoji,conteo))
+    return resultado
+
